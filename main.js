f8e29f81b5e3e3595c56a865e35d912f81d09e45
Milena Emmert <milena.emmert@outlook.com>
2022-10-15 23:30:34 -0300

    Change <li> by <button> and creating logic to capture id  and add or remove color

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
-const pontuacao = document.getElementById('nota')
-console.log(pontuacao)
-
+//button submit
 const submit = document.getElementById('submit')
-console.log(submit)
+//console.log(submit)
+
+//Here I am taking the feedback buttons by class
+const feedbackButtons = document.querySelectorAll('.buttons')
+
+//This is my control variable
+let clickedFeedbackButton
+
+/* the forEach method is a loop, like a for or a while, we can use it in arrays or in a NodeList, like in this case
+
+The querySelectorAll() returns a NodeList, I can show you more about it later! =)
+*/
+feedbackButtons.forEach(feedbackButton => {
+  
+  //click event, maybe we should rename some names and elements here, to make it easier to understand
+  feedbackButton.addEventListener('click', () => {
+    clickedFeedbackButton = feedbackButton.id
+    console.log(clickedFeedbackButton) //open console on your browser and check if is capturing the correct id
+
+    //first of all we reset all buttons (just in case if some already has this class)
+    feedbackButtons.forEach(button => {
+      button.classList.remove('button-selected')
+    })
+
+    //then we add the class to the clicked button
+    feedbackButton.classList.add('button-selected')
+
+  })
+})
